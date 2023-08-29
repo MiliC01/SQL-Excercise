bef20da8077c7da1a3c6cd25759d09bc98cd6557
Mili Cortez <126219366+MiliC01@users.noreply.github.com>
2023-08-28 19:28:21 -0600

    Update ProjectBasics.sql

--- ProjectBasics.sql
+++ ProjectBasics.sql
@@ -1,3 +1,4 @@
+-- Creating two data tables, one with Employee Demographics and another one with Employee Salary data
 CREATE TABLE EmployeeDemographics
 (EmployeeID int,
 FirstName Varchar(50),
@@ -11,10 +12,11 @@ CREATE TABLE EmployeeSalary
 JobTitle varchar(50),
 Salary int
 )
--- Inserting data
+-- Inserting data to both data tables
 INSERT INTO EmployeeDemographics VALUES
 (1001, 'Jim', 'Halpert', 30, 'Male')
 
+-- Note that there is duplicate data we have to delete 
 INSERT INTO EmployeeDemographics VALUES
 (1001, 'Jim', 'Halpert', 30, 'Male'),
 (1002, 'Pam', 'Beasley', 30, 'Female'),
@@ -51,3 +53,122 @@ WITH CTE AS(
 	FROM EmployeeSalary
 )
 DELETE FROM CTE WHERE RN > 1
+
+/*
+Basic Functions
+Select Statement
+*, Top, Distinct, Count, As, Max, Min, Avg
+*/
+
+-- Selecting all the table 
+SELECT *
+FROM EmployeeDemographics
+-- Selecting the first five rows of the table
+SELECT TOP 5 *
+FROM EmployeeDemographics
+
+SELECT DISTINCT(Gender)
+FROM EmployeeDemographics
+
+SELECT COUNT(LastName)AS LastNameCount
+FROM EmployeeDemographics
+
+SELECT MAX(Salary)
+FROM EmployeeSalary
+
+SELECT MIN(Salary)
+FROM EmployeeSalary
+
+SELECT AVG(Salary)
+FROM EmployeeSalary
+
+/*
+Basic Functions
+Where Statement (limits the amount of data, and specifies what data you want returned)
+=, <>, <, >, And, Or, Like, Null, Not Null, In
+*/
+
+-- = -> equals
+SELECT *
+FROM EmployeeDemographics
+WHERE FirstName = 'Jim'
+
+-- <> -> does not equal
+SELECT *
+FROM EmployeeDemographics
+WHERE FirstName <> 'Jim'
+
+-- greater than
+SELECT *
+FROM EmployeeDemographics
+WHERE Age > 30
+
+-- Greater than or equal
+SELECT *
+FROM EmployeeDemographics
+WHERE Age >= 30
+
+SELECT *
+FROM EmployeeDemographics
+WHERE Age < 32
+
+SELECT *
+FROM EmployeeDemographics
+WHERE Age <= 32
+-- Use the AND statement
+SELECT *
+FROM EmployeeDemographics
+WHERE Age <= 32 AND Gender = 'Male'
+
+-- find the last names that start with S
+SELECT *
+FROM EmployeeDemographics
+WHERE LastName LIKE 'S%'
+
+-- find the last names that start with S and has o
+SELECT *
+FROM EmployeeDemographics
+WHERE LastName LIKE 'S%o%'
+
+-- Null and Not Null
+SELECT *
+FROM EmployeeDemographics
+WHERE FirstName is NULL
+
+SELECT *
+FROM EmployeeDemographics
+WHERE FirstName is NOT NULL
+
+-- Using In statement
+
+SELECT *
+FROM EmployeeDemographics
+WHERE FirstName IN ('Jim', 'Michael')
+
+/*
+Basic Functions
+Group By, Order By
+*/
+-- Similar to Distinct in this case
+SELECT Gender
+FROM EmployeeDemographics
+GROUP BY Gender
+
+-- how to see the difference
+SELECT Gender, COUNT(Gender)
+FROM EmployeeDemographics
+GROUP BY Gender
+
+--Adding a WHERE clause
+SELECT Gender, COUNT(Gender)
+FROM EmployeeDemographics
+WHERE Age > 31
+GROUP BY Gender
+
+---- Using Order By
+SELECT Gender, COUNT(Gender) AS CountGender
+FROM EmployeeDemographics
+WHERE Age > 31
+GROUP BY Gender
+ORDER BY CountGender DESC
+
